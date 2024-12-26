65bc414cc34b445f853e46cfcbc5c04af8214bb8
Awais-Chandio <awaischandio851@gmail.com>
2024-12-26 15:44:41 +0500

    initial
    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'core/app_export.dart';
+import 'core/utils/size_utils.dart';
+import 'presentation/signup_mob_screen.dart';
+
+
+var globalMessengerKey = GlobalKey<ScaffoldMessengerState>();
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Future.wait([
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]),
+    PrefUtils().init()
+  ]).then((value) {
+    runApp(MyApp());
+  });
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+        return ChangeNotifierProvider(
+          create: (context) => ThemeProvider(),
+          child: Consumer<ThemeProvider>(
+            builder: (context, provider, child) {
+              return MaterialApp(
+                theme: theme,
+                title: 'bidbuyy',
+                // navigatorKey: NavigatorService.navigatorKey,
+                debugShowCheckedModeBanner: false,
+                localizationsDelegates: const [
+                  AppLocalizationDelegate(),
+                  // GlobalMaterialLocalizations.delegate,
+                  // GlobalWidgetsLocalizations.delegate,
+                  // GlobalCupertinoLocalizations.delegate,
+                ],
+                supportedLocales: const [
+                  Locale(
+                    'en',
+                    '',
+                  ),
+                ],
+                home: SignupMobScreen(),
+                
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
+}
